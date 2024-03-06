6f1b7ee31d99314f992996d5c54b14327f5164f5
André Reis <arrr.cin@gmail.com>
2024-03-06 13:10:49 -0300

    Remove references to example pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,22 +17,9 @@ import routerBindings, {
   NavigateToResource,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { Header } from "./components/header";
 import { ColorModeContextProvider } from "./contexts/color-mode";
-import {
-  BlogPostCreate,
-  BlogPostEdit,
-  BlogPostList,
-  BlogPostShow,
-} from "./pages/blog-posts";
-import {
-  CategoryCreate,
-  CategoryEdit,
-  CategoryList,
-  CategoryShow,
-} from "./pages/categories";
 import { SaleList, SaleShow } from "./pages/sales";
 import { productDbDataProvider } from "./providers/product-db-data-provider";
 import { ProductList, ProductShow } from "./pages/products";
@@ -49,33 +36,10 @@ function App() {
           <RefineSnackbarProvider>
             <DevtoolsProvider>
               <Refine
-                dataProvider={{
-                  default: dataProvider("https://api.fake-rest.refine.dev"),
-                  productDb: productDbDataProvider(),
-                }}
+                dataProvider={productDbDataProvider()}
                 notificationProvider={notificationProvider}
                 routerProvider={routerBindings}
                 resources={[
-                  {
-                    name: "blog_posts",
-                    list: "/blog-posts",
-                    create: "/blog-posts/create",
-                    edit: "/blog-posts/edit/:id",
-                    show: "/blog-posts/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                  {
-                    name: "categories",
-                    list: "/categories",
-                    create: "/categories/create",
-                    edit: "/categories/edit/:id",
-                    show: "/categories/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
                   {
                     name: "sales",
                     list: "/sales",
@@ -90,7 +54,6 @@ function App() {
                     list: "/products",
                     show: "/products/show/:id",
                     meta: {
-                      dataProviderName: "productDb",
                       canDelete: false,
                     },
                   },
@@ -129,18 +92,6 @@ function App() {
                       index
                       element={<NavigateToResource resource="products" />}
                     />
-                    <Route path="/blog-posts">
-                      <Route index element={<BlogPostList />} />
-                      <Route path="create" element={<BlogPostCreate />} />
-                      <Route path="edit/:id" element={<BlogPostEdit />} />
-                      <Route path="show/:id" element={<BlogPostShow />} />
-                    </Route>
-                    <Route path="/categories">
-                      <Route index element={<CategoryList />} />
-                      <Route path="create" element={<CategoryCreate />} />
-                      <Route path="edit/:id" element={<CategoryEdit />} />
-                      <Route path="show/:id" element={<CategoryShow />} />
-                    </Route>
                     <Route path="/sales">
                       <Route index element={<SaleList />} />
                       <Route path="show/:id" element={<SaleShow />} />

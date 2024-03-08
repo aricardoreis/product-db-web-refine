56b6e273aaaa6c9d610653e0b2f6c0559a70b012
André Reis <arrr.cin@gmail.com>
2024-03-07 21:10:22 -0300

    [deploy] Update chunk split approach

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,27 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // adjusting chunkSizeWarningLimit only because @mui lib is huge
+    chunkSizeWarningLimit: 560,
+    rollupOptions: {
+      output: {
+        manualChunks: (id: string) => splitByGroups(id),
+      },
+    },
+  },
 });
+
+const splitByGroups = (id: string) => {
+  const chunkBigGroups = ["@mui", "@zxing", "@refinedev", "react-dom"];
+  if (id.includes("node_modules")) {
+    return chunkBigGroups.find((group) => id.includes(group)) ?? "vendor";
+  }
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const splitByModules = (id: string) => {
+  if (id.includes('node_modules')) return id.toString().split('node_modules/')[1].split('/')[0].toString();
+};
+
+
